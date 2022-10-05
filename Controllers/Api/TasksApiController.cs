974ccbf9ff1259a13937a596ddfbb66c44f70ded
Albert Khurshudyan <albert.khurshudyan96@gmail.com>
2022-10-05 23:34:17 +0300

    web api

---
+++ Controllers/Api/TasksApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using TodoList.Infrastructure;
+using TodoList.Models;
+using TodoList.Services;
+
+namespace TodoList.Controllers.Api
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TasksApiController : ControllerBase
+    {
+        private readonly IToDoService toDoService;
+
+        public TasksApiController(IToDoService tasks)
+            => this.toDoService = tasks;
+
+        
+        [HttpGet]
+        [Route("all")]
+        public AllToDosQueryModel AllTasks()
+        {
+            var userId = User.GetId();
+
+            return this.toDoService.All(userId);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public ToDoFormModel Delete(int id)
+        {
+            return this.toDoService.Delete(id);
+        }
+    }
+}
